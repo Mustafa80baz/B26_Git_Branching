697dca77e71478535851ed6139531e6f068379bf
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-09 16:03:24 +0300

    Kubra dosyası dosya Guncellendi 16:4

--- src/BazDosya/Kubra.java
+++ src/BazDosya/Kubra.java
@@ -6,6 +6,10 @@ public class Kubra {
         Nihalin diger adı bu
         Beni kandır ma
         Saat :16 :01
+
+        Kaydettik ama ekleme yapmamız lazım
+        Yapalım ozaman
+        Saat:16 04
          */
     }
 }
