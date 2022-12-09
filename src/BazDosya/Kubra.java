88aa83905b01a36d8d09234314487c6e11416e1f
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-09 16:02:25 +0300

    Kubra dosyası Yazıldı

---
+++ src/BazDosya/Kubra.java
@@ -0,0 +1,11 @@
+package BazDosya;
+
+public class Kubra {
+    public static void main(String[] args) {
+        /*
+        Nihalin diger adı bu
+        Beni kandır ma
+        Saat :16 :01
+         */
+    }
+}
